17a3af1c61f790f6f8481e5685d1c0a7e8b548ce
Magma <sacred.lunar.dragon@gmail.com>
2024-01-02 23:05:45 +0100

    Finished the mod.

---
+++ src/main/java/net/magmabits/hydrosteel/item/ModArmorItem.java
@@ -0,0 +1,82 @@
+package net.magmabits.hydrosteel.item;
+
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.Map;
+
+public class ModArmorItem extends ArmorItem {
+    private static final Map<ArmorMaterial, StatusEffect> MATERIAL_TO_EFFECT_MAP =
+            (new ImmutableMap.Builder<ArmorMaterial, StatusEffect>())
+                    .put(ModArmorMaterial.HYDROSTEEL, StatusEffects.CONDUIT_POWER).build();
+
+    public ModArmorItem(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
+        super(material, slot, settings);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if(!world.isClient()) {
+            if(entity instanceof PlayerEntity) {
+                PlayerEntity player = (PlayerEntity)entity;
+
+                if(hasFullSuitOfArmor(player)) {
+                    evaluateArmorEffects(player);
+                }
+            }
+        }
+
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    private void evaluateArmorEffects(PlayerEntity player) {
+        for (Map.Entry<ArmorMaterial, StatusEffect> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
+            ArmorMaterial mapArmorMaterial = entry.getKey();
+            StatusEffect mapStatusEffect = entry.getValue();
+
+            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
+                if(player.isTouchingWater()) {
+                    addStatusEffectForMaterial(player, mapArmorMaterial, StatusEffects.CONDUIT_POWER);
+                    addStatusEffectForMaterial(player, mapArmorMaterial, StatusEffects.DOLPHINS_GRACE);
+                } else {
+                    addStatusEffectForMaterial(player, mapArmorMaterial, StatusEffects.WEAKNESS);
+                }
+            }
+        }
+    }
+
+    private void addStatusEffectForMaterial(PlayerEntity player, ArmorMaterial mapArmorMaterial, StatusEffect mapStatusEffect) {
+        boolean hasPlayerEffect = player.hasStatusEffect(mapStatusEffect);
+
+        if(hasCorrectArmorOn(mapArmorMaterial, player) && !hasPlayerEffect) {
+            player.addStatusEffect(new StatusEffectInstance(mapStatusEffect, 400, 0, false, false, true));
+        }
+    }
+
+    private boolean hasFullSuitOfArmor(PlayerEntity player) {
+        ItemStack boots = player.getInventory().getArmorStack(0);
+        ItemStack leggings = player.getInventory().getArmorStack(1);
+        ItemStack chestplate = player.getInventory().getArmorStack(2);
+        ItemStack helmet = player.getInventory().getArmorStack(3);
+
+        return !helmet.isEmpty() && !chestplate.isEmpty() && !leggings.isEmpty() && !boots.isEmpty();
+    }
+
+    private boolean hasCorrectArmorOn(ArmorMaterial material, PlayerEntity player) {
+        ArmorItem boots = ((ArmorItem)player.getInventory().getArmorStack(0).getItem());
+        ArmorItem leggings = ((ArmorItem)player.getInventory().getArmorStack(1).getItem());
+        ArmorItem chestplate = ((ArmorItem)player.getInventory().getArmorStack(2).getItem());
+        ArmorItem helmet = ((ArmorItem)player.getInventory().getArmorStack(3).getItem());
+
+        return helmet.getMaterial() == material && chestplate.getMaterial() == material && leggings.getMaterial() == material && boots.getMaterial() == material;
+    }
+}

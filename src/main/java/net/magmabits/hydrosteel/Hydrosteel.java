55bbc6594e75eb2bcaad30f6695e8062e772f61e
Magma <sacred.lunar.dragon@gmail.com>
2024-01-02 16:17:09 +0100

    Initial commit

---
+++ src/main/java/net/magmabits/hydrosteel/Hydrosteel.java
@@ -0,0 +1,16 @@
+package net.magmabits.hydrosteel;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class Hydrosteel implements ModInitializer {
+	public static final String MOD_ID = "hydrosteel";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/magmabits/hydrosteel/HydrosteelClient.java
@@ -0,0 +1,10 @@
+package net.magmabits.hydrosteel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HydrosteelClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
